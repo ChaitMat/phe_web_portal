97e82e79cb20215e1c306b71ed57648f9a111564
Chaitanya Mathkar <chaitanya.mathkar@gmail.com>
2019-09-30 23:21:01 +0530

    Delete booking WIP

--- portal/urls.py
+++ portal/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('events/<int:pk>', views.event_detail_view, name='event-detail'),
     path('events/<int:pk>/edit', views.edit_event_form, name='event-edit'),
     path('events/<int:pk>/delete', views.delete_event, name='event-delete'),
-    # path('events/<int:pk>/booking', views.add_bookings, name='booking-new'),
+    path('events/<int:pk>/booking/', views.add_bookings, name='booking-new'),
+    path('events/<int:pk>/booking/<int:bk>/edit', views.edit_booking, name='booking-edit'),
+    path('events/<int:pk>/booking/<int:bk>/delete', views.delete_booking, name='booking-delete'),
 ]
--- portal/views.py
+++ portal/views.py
@@ -13,7 +13,7 @@ from .models import Event, Booking
 
 from portal.forms import NewEventForm, NewBookingForm
 
-        
+# Index page
 def eventList(request):
     
     events = Event.objects.all()
@@ -26,7 +26,7 @@ def eventList(request):
     
     return render(request, 'portal/event_list.html', context=context)
 
-
+# New event form
 def new_event_form(request):
 
     if request.method == "POST":
@@ -49,41 +49,12 @@ def new_event_form(request):
 
     return render(request, 'portal/event_new_form.html', context)
 
-
+# View details of an event 
 def event_detail_view(request, pk):
     
     event = get_object_or_404(Event, pk=pk)
 
     form = NewBookingForm()
-
-    if request.method == 'POST'and request.is_ajax():
-
-        form = NewBookingForm(request.POST)
-
-        if form.is_valid():
-
-            b = form.save(commit=False)
-
-            event.bookings.create(
-                            booked_by = b.booked_by,
-                            participants_count = b.participants_count,
-                            contact_number = b.contact_number,
-                            date_of_payment = b.date_of_payment,
-                            mode_of_payment = b.mode_of_payment,
-                        )
-
-            total_participants = event.bookings.all().aggregate(Sum('participants_count'))['participants_count__sum']
-
-            response_data = {}
-
-            response_data['booked_by'] = b.booked_by
-            response_data['participants_count'] = b.participants_count
-            response_data['contact_number'] = b.contact_number
-            response_data['date_of_payment'] = b.date_of_payment.strftime('%B %-d, %Y')
-            response_data['mode_of_payment'] = b.mode_of_payment
-            response_data['total_participants'] = total_participants
-            
-            return HttpResponse(json.dumps(response_data), content_type="application/json")
 
     bookings = event.bookings.all()
 
@@ -98,7 +69,7 @@ def event_detail_view(request, pk):
     
     return render(request, 'portal/event_detail.html', context)
 
-
+# Edit an existing event
 def edit_event_form(request, pk):
 
     event = get_object_or_404(Event, pk=pk)
@@ -130,7 +101,7 @@ def edit_event_form(request, pk):
 
     return render(request, 'portal/event_edit_form.html', context)
 
-
+# Delete an existing event
 def delete_event(request, pk):
 
     event = get_object_or_404(Event, pk=pk)
@@ -138,28 +109,88 @@ def delete_event(request, pk):
 
     return HttpResponseRedirect(reverse('index'))
 
+# Add bookings to an existing event
+def add_bookings(request, pk):
 
-# def add_bookings(request, pk):
+    event = get_object_or_404(Event, pk=pk)
 
-#     event = get_object_or_404(Event, pk=pk)
+    form = NewBookingForm()
 
-#     if request.method == 'POST':
+    if request.method == 'POST'and request.is_ajax():
 
-#         form = NewBookingForm(request.POST)
+        form = NewBookingForm(request.POST)
 
-#         print(form)
+        if form.is_valid():
 
-#         # if form.is_valid():
+            b = form.save(commit=False)
 
-#         #     event = form.save()
+            new_booking = event.bookings.create(
+                            booked_by = b.booked_by,
+                            participants_count = b.participants_count,
+                            contact_number = b.contact_number,
+                            date_of_payment = b.date_of_payment,
+                            mode_of_payment = b.mode_of_payment,
+                        )
 
-#         #     event.save()
+            total_participants = event.bookings.all().aggregate(Sum('participants_count'))['participants_count__sum']
 
-#         return HttpResponseRedirect(reverse('index') )
+            response_data = {}
 
+            response_data['booked_by'] = b.booked_by
+            response_data['participants_count'] = b.participants_count
+            response_data['contact_number'] = b.contact_number
+            response_data['date_of_payment'] = b.date_of_payment.strftime('%B %-d, %Y')
+            response_data['mode_of_payment'] = b.mode_of_payment
+            response_data['total_participants'] = total_participants
+            response_data['pk'] = pk
+            response_data['bk'] = new_booking.pk
+            
+            return HttpResponse(json.dumps(response_data), content_type="application/json")
 
+# Edit bookings from an event
+def edit_booking(request, pk, bk):
 
+    event = get_object_or_404(Event, pk=pk)
 
+    booking = get_object_or_404(Booking, pk=bk)
+
+    date = booking.date_of_payment
+
+    booking.date_of_payment = date.strftime("%d/%m/%Y")
+
+    if request.method == "POST":
+
+        form = NewBookingForm(request.POST, instance = booking)
+
+        if form.is_valid():
+
+            booking = form.save()
+
+            booking.save()
+
+            return HttpResponseRedirect(reverse('event-detail', args=(event.pk,)))
+
+    else:
+
+        form = NewBookingForm(instance = booking)
+
+    context = {
+        'form' : form,
+        'booking' : booking,
+        'event' : event
+        }
+
+    return render(request, 'portal/booking_edit_form.html', context)
+
+# Delete an existing booking
+def delete_booking(request, bk):
+
+    booking = get_object_or_404(Booking, pk=bk)
+    booking.delete()
+
+    return HttpResponseRedirect(reverse('index'))
+
+   
 
 
 
